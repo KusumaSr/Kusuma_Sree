e722a1c40a244692078b10a0270acccc5d07156a
KusumaSr <109414204+KusumaSr@users.noreply.github.com>
2022-11-10 17:30:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css"
+import FormInput from "./components/FormInput"
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <forms>
+        <FormInput placeholder="Username"/>
+        <FormInput placeholder="Email"/>
+        <FormInput placeholder="Full Name/>
+        <FormInput placeholder="/>
+      </forms>
+    </div>
+  );
+}
+
+export default App;
